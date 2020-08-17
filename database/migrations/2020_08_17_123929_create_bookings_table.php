90406d8c7504d0dce57f25c43f7e3005ca39bb0b
Ainee Chong <ainee.chong@gem.live>
2020-08-17 20:52:51 +0800

    Database migration

---
+++ database/migrations/2020_08_17_123929_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id('booking_id');
+            $table->timestamp('created_at_local');
+            $table->integer('driver_id')->unsigned();
+            $table->integer('passenger_id')->unsigned();
+            $table->string('state');
+            $table->integer('country_id')->unsigned();
+            $table->decimal('fare', 8, 2)->default(0.00);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
