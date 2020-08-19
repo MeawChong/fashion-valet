adea6ebb76f7b5c49bc57cd61270fc38dec19e3a
Ainee Chong <ainee.chong@gem.live>
2020-08-20 00:12:52 +0800

    Added routes and form packages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'driver'], function () {
+    Route::get('/', ['as' => 'index', 'uses' => 'DriverController@index']);
+    Route::post('/', ['as' => 'show', 'uses' => 'DriverController@show']);
+});
+
+Route::group(['prefix' => 'report'], function () {
+    Route::get('/', ['as' => 'index', 'uses' => 'ReportController@index']);
+    Route::post('/', ['as' => 'filter', 'uses' => 'ReportController@filter']);
+});
+
+Route::group(['prefix' => 'address'], function () {
+    Route::get('/', ['as' => 'index', 'uses' => 'AddressController@index']);
+    Route::post('/', ['as' => 'format', 'uses' => 'AddressController@format']);
+});
